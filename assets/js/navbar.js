221c5b3d8ffd596efdcb81c224ea6a4cca6540b0
bai-sajewellers <88672518+bai-sajewellers@users.noreply.github.com>
2021-08-09 20:34:50 +0530

    Add files via upload

---
+++ assets/js/navbar.js
@@ -0,0 +1,19 @@
+const toggleMenu = document.getElementsByClassName('toggle-menu')[0]
+const links = document.getElementsByClassName('links')[0]
+const menu = document.getElementsByClassName('menu')[0]
+const main = document.getElementsByClassName('main')[0]
+var click = 0
+
+toggleMenu.addEventListener('click', () => {
+    links.classList.toggle('active');
+    if (click % 2 == 0) {
+        menu.style.display = 'none';
+        main.style.marginTop = '0';
+    }
+    else {
+        menu.style.display = 'flex';
+        main.style.marginTop = '6rem';
+    }
+    click++;
+    
+})
