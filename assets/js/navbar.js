7f5c6764d2f3b9b79d964a229c175555d8886b24
bai-sajewellers <88672518+bai-sajewellers@users.noreply.github.com>
2021-08-10 17:03:55 +0530

    Add files via upload

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,7 +2,7 @@ const toggleMenu = document.getElementsByClassName('toggle-menu')[0]
 const links = document.getElementsByClassName('links')[0]
 const menu = document.getElementsByClassName('menu')[0]
 const main = document.getElementsByClassName('main')[0]
-var click = 0
+var click = 1
 
 toggleMenu.addEventListener('click', () => {
     links.classList.toggle('active');
